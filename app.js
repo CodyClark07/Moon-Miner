a53e143cb9be37ec7b168f233cf15ece13019056
CodyClark07 <codyclark2010@hotmail.com>
2020-07-01 14:36:50 -0600

    First Draft-fully functional

--- app.js
+++ app.js
@@ -1,16 +1,192 @@
-//#region Game Logic & Data
+//  Game Logic & Data
 
 // Data //
-let moonRockCount = 0
+let players = [];
+let moonRockCount = 0;
+let multipliedRate = 0;
+let autoIntervalClock = false;
+let upgradeItems = {
+    shovel: {
+        name: "shovel",
+        price: 10,
+        quantity: 1,
+        multiplier: 1,
+        upgradeIncrement: 1.3
+    },
+    drill: {
+        name: "drill",
+        price: 40,
+        quantity: 0,
+        multiplier: 5,
+        upgradeIncrement: 1.25
+    },
+    laser: {
+        name: "laser",
+        price: 80,
+        quantity: 0,
+        multiplier: 10,
+        upgradeIncrement: 1.5
+    },
+    rover: {
+        name: "rover",
+        price: 100,
+        quantity: 0,
+        multiplier: 20,
+        upgradeIncrement: 1.75
+    }
+}
+//#region  Game Logic- Sign In/Load
 
+function generateID() {
+    return (
+        Math.floor(Math.random() * 10000000) +
+        "-" +
+        Math.floor(Math.random() * 10000000)
+    );
+}
 
-function mine() {
-    moonRockCount++
+function savePlayer() {
+    window.localStorage.setItem("player", JSON.stringify(players))
+    let playerElem = document.getElementById("player-name")
+    let playerName = ""
+    players.forEach(player => {
+        playerName += `<h3 class="text-white text-center"> Player Name: ${player.name}`
+    })
+    playerElem.innerHTML = playerName
+}
+
+function signIn(event) {
+    event.preventDefault()
+    let form = event.target
+
+    let player = {
+        id: generateID(),
+        name: form.name.value
+    }
+    if (form.name.value == "") {
+        alert("Must Enter Name")
+    }
+    else {
+        players.push(player)
+        savePlayer()
+    }
+
+    form.reset()
+}
+function loadGame() {
+    document.getElementById("sign-in").classList.add("hidden")
+    document.getElementById("game-play").classList.remove("hidden")
+    document.getElementById("stat-card").classList.remove("hidden")
+    drawInventory()
+}
+//#endregion
+
+//#region Game Logic- GamePlay
+function mine(input) {
+    multipliedRate = upgradeItems[input].quantity * upgradeItems[input].multiplier;
+
+    if (multipliedRate == 0) {
+        moonRockCount++
+    }
+    else if (multipliedRate > 0) {
+        moonRockCount += multipliedRate
+
+        console.log(`${moonRockCount}`);
+
+    }
     draw()
+}
+
+function upgrade(input) {
+    let upgradeSelect = upgradeItems[input]
+    if (upgradeSelect.price <= moonRockCount && upgradeItems[input] !== "shovel") {
+        upgradeSelect.quantity++;
+        moonRockCount -= upgradeSelect.price
+        multipliedRate += upgradeSelect.multiplier
+        upgradeSelect.price *= upgradeSelect.upgradeIncrement;
+
+    }
+    else if (upgradeSelect.price <= moonRockCount && input == "shovel") {
+        upgradeSelect.quantity++;
+        moonRockCount -= upgradeSelect.price
+        upgradeSelect.price *= upgradeSelect.upgradeIncrement;
+
+    }
+
+    else {
+        alert(`Could not afford, need more moon rocks (${upgradeSelect.price - moonRockCount})`)
+    }
+
+    drawInventory()
+    drawMRPS()
+    draw()
+}
+
+
+function autoMine() {
+    for (let key in upgradeItems) {
+        if (upgradeItems[key].quantity > 0 && key !== 'shovel') {
+            moonRockCount += upgradeItems[key].quantity * upgradeItems[key].multiplier
+        }
+        draw()
+    }
+}
+
+function autoInterval() {
+    if (autoIntervalClock == false) {
+        setInterval(autoMine, 3000);
+    }
+    else {
+
+    }
+}
+//#endregion
+
+//#region Game Logic- Drawing
+function drawInventory() {
+    let inventoryCard = document.getElementById('inventory-card')
+    inventoryCard.innerHTML = ''
+    inventoryCard.innerHTML += `
+           <h3 id="shovel"> ${ upgradeItems.shovel.quantity} </h3>
+           <h3 id="drill">${ upgradeItems.drill.quantity} </h3>
+           <h3 id="laser">${ upgradeItems.laser.quantity} </h3>
+           <h3 id="rover">${ upgradeItems.rover.quantity} </h3>`
+}
+
+function drawMRPS() {
+    let mrpsCard = document.getElementById("mrps-card")
+    mrpsCard.innerHTML = ''
+    for (let key in upgradeItems) {
+        if (upgradeItems[key].quantity > 0 && key !== 'shovel') {
+            mrpsCard.innerHTML += `
+            <div><h3>${upgradeItems[key].name} - ${upgradeItems[key].quantity}: ${Math.floor((upgradeItems[key].multiplier) *
+                (upgradeItems[key].quantity) / 3)} MRPS</h3></div>`
+        }
+    }
+}
+
+function drawUpgrades() {
+    let upgradesAvailable = document.getElementById("upgrades-available")
+    upgradesAvailable.innerHTML = ''
+    for (let key in upgradeItems) {
+        if (moonRockCount >= 10) {
+            upgradesAvailable.innerHTML += `  
+                <div class="col-3 col-md-3 ">
+                <button id="${upgradeItems[key].name}" class="btn btn-warning btn-lg"
+                        onclick="upgrade('${key}')">${upgradeItems[key].name}: <br> $ ${Math.floor(Math.ceil(upgradeItems[key].price))}</button></div>`
+
+        }
+
+    }
+
 }
 
 function draw() {
     let clickCountElem = document.getElementById("moon-rock-count")
 
-    clickCountElem.innerHTML = moonRockCount.toString()
+    clickCountElem.innerText = Math.floor(moonRockCount)
+    drawUpgrades()
 }
+draw()
+autoInterval()
+//#endregion
